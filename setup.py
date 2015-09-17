924c9db8a6759872097bb08cc704f10f78b0f84c
sc4reful <scare.development@gmail.com>
2015-09-16 18:56:42 -0700

    First release, do pip install dscsrf for CSRF protection

--- setup.py
+++ setup.py
@@ -1,11 +1,23 @@
 from distutils.core import setup
+import os, sys
+
 setup(name='dscsrf',
 	version='1.0',
-	description='Global double-submit Flask CSRF',
-	packages=['dscsrf'],
- 	py_modules=['flask'],
+	url='https://github.com/sc4reful/dscsrf',
+	license='MIT',
 	author='sc4reful',
-	url = 'https://github.com/sc4reful/dscsrf',
-	keywords = ['security', 'flask', 'website', 'csrf'],
-	download_url = 'https://github.com/sc4reful/dscsrf/tarball/1.0',
+	author_email='notanemail@insorg-mail.info',
+	description='Double-submit CSRF protection for Flask applications.',
+	py_modules=['dscsrf'],
+	zip_safe=False,
+	platforms='any',
+	install_requires=['Flask'],
+	classifiers = [
+		'Environment :: Web Environment',
+		'Intended Audience :: Developers',
+		'License :: OSI Approved :: MIT License',
+		'Operating System :: OS Independent',
+		'Programming Language :: Python',
+		'Topic :: Software Development :: Libraries :: Python Modules'
+	],
 )
