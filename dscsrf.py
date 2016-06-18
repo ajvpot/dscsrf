735a719ebe4e4dddc172cd8c1f1601254229a16e
Alex Vanderpot <alexjvanderpotwebdesign@gmail.com>
2016-06-18 14:04:43 -0500

    Fix response object handling, add temp param
    Temp param for if the cookie was created w/ this request.

--- dscsrf.py
+++ dscsrf.py
@@ -1,18 +1,18 @@
 import string
-from flask import request
 from random import SystemRandom
+
+from flask import request, after_this_request
 from markupsafe import Markup
 
 csrf_charset = string.letters + string.digits
-
 _rng = SystemRandom()
+
 
 class Csrf(object):
 	def __init__(self, app=None, paramName='dscsrf', cookieName='dscsrf'):
 		self.app = None
 		self.paramName = paramName
 		self.cookieName = cookieName
-
 		if app is not None:
 			self.init_app(app, paramName, cookieName)
 
@@ -20,19 +20,28 @@ class Csrf(object):
 		self.app = app
 		self.paramName = paramName
 		self.cookieName = cookieName
-
 		self.app.before_request(self.checkCSRFCookie)
 		self.app.jinja_env.globals['csrf_token'] = self.csrf_token
 
 	def csrf_token(self):
-		return Markup('<input type="hidden" name="{0}" value="{1}" />'.format(self.paramName, request.cookies.get(self.cookieName)))
-
+		if hasattr(request, '_tmp_csrf'):
+			token = request._tmp_csrf
+		else:
+			token = request.cookies.get(self.cookieName)
+		return Markup('<input type="hidden" name="{0}" value="{1}" />'.format(self.paramName, token))
 
 	def checkCSRFCookie(self):
-		self.setCSRFCookie()
+		cookie = request.cookies.get(self.cookieName, None)
+		if cookie is None:
+			cookie = ''.join([_rng.choice(csrf_charset) for _ in xrange(64)])
+			request._tmp_csrf = cookie  # hacks
+
+			@after_this_request
+			def setCookie(response):
+				response.set_cookie(self.cookieName, cookie)
+				return response
 		if request.method != 'POST':
 			return
-		cookie = request.cookies.get(self.cookieName, None)
 		if cookie is None:
 			return 'CSRF cookie not set.', 403
 		form = request.form.get(self.paramName, None)
@@ -40,10 +49,3 @@ class Csrf(object):
 			return 'CSRF form value not set.', 403
 		if cookie != form:
 			return 'CSRF token invalid.', 403
-
-	def setCSRFCookie(self, response):
-		rc = request.cookies.get(self.cookieName, None)
-		if rc is None:
-			cookie = ''.join([_rng.choice(csrf_charset) for _ in xrange(64)])
-			response.set_cookie(self.cookieName, cookie)
-		return response
